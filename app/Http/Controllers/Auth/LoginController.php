a157b163434e9d0cab061fb1b421e6535b755ed0
kienttpd07844 <kienttpd07844@fpt.edu.vn>
2024-10-03 06:40:49 +0700

    Bổ sung giỏ hàng khách khi đăng nhập

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginFormRequest;
+use App\Models\Cart;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,7 +26,24 @@ class LoginController extends Controller
         User::where('id', Auth::user()->id)->update([
             'ip_address' => $request->ip(),
         ]);
-
+        if (isset($_COOKIE['guest_user_id'])) {
+            $carts  = Cart::where('guest_user_id', request()->cookie('guest_user_id'))->get();
+            $userId = auth()->user()->id;
+            if ($carts) {
+                foreach ($carts as $cart) {
+                    $existInUserCart = Cart::where('user_id', $userId)->where('product_id', $cart->product_id)->first();
+                    if (!is_null($existInUserCart)) {
+                        $existInUserCart->qty += $cart->qty;
+                        $existInUserCart->save();
+                        $cart->delete();
+                    } else {
+                        $cart->user_id = $userId;
+                        $cart->guest_user_id = null;
+                        $cart->save();
+                    }
+                }
+            }
+        }
         return redirect()->route('home')->with(noti('Đăng nhập thành công', 'success'));
     }
 
